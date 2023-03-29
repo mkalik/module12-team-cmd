0f4b5edd4d60544143aefa2583327fbc5be2dc66
mkalik <mbadiankouyate@gmail.com>
2023-03-29 12:21:23 -0700

    added an ascii header

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 //wanted to keep things rather uncluttered so this file strictly handles starting the app
 //seems a little pointless but I wanted to practice modularizing things
 require('dotenv').config();
+let { header } = require('./lib/header.js');
 const { questions } = require('./dist/menuQuestions.js');
 function init() {
+    console.log(header);
     questions();
 }
 init();
